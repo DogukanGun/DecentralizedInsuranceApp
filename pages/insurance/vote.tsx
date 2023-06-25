827b5a8a80b5147b8eef02b8a81145628de08800
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-06-25 14:29:06 +0300

    Vote screen

--- pages/insurance/vote.tsx
+++ pages/insurance/vote.tsx
@@ -4,7 +4,22 @@ import CustomTitle from "../../components/title/title";
 const Vote = () => {
     return (
         <Page>
-            <CustomTitle title="Hello, Let&apos;s Vote"/>
+            <CustomTitle title="Hello, Let&apos;s Vote" />
+            <div className="flex w-full h-96 mt-20">
+                <div className="w-1/2 h-full bg-black mx-10">
+                    A
+                </div>
+                <div className="col-span-12 w-1/2 h-full mx-10">
+                    <div className="flex w-full text-black justify-center mt-10">
+                        Asset Value : 1200 stNear
+                        Wallet Address: 0x1111
+                    </div>
+                    <div className="flex w-96 justify-around mt-20 mx-auto">
+                        <button className="btn btn-error">No</button>
+                        <button className="btn btn-success">Yes</button>
+                    </div>
+                </div>
+            </div>
         </Page>
     )
 }
