45daf2e5ce3a8af1c050f055d699e9bf5c953725
cemdenizsel <cem.denizsel@gmail.com>
2023-06-25 16:56:10 +0200

    Voting contract updated.

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,34 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./SolInsurance.sol";
 
 contract Voting {
-    mapping(address => bool) public hasVoted;
-    uint256 public approvalThreshold;
-    address public generatedContract;
-    uint256 public voteFee;
-    TotalVote[] votes;
-    bool public hasActiveVote;
-
-    //TODO vaiting votes can be added here
-    
-    struct Asset{
+    struct Asset {
         address ownerWallet;
         uint256 assetValue;
     }
 
-    struct TotalVote {
+    struct Vote {
         Asset asset;
         uint256 totalVotes;
         uint256 positiveVotes;
+        address insuranceContract;
+        bool votingCompleted;
     }
 
-    
+    struct VotingRequest {
+        address userWallet;
+        uint256 assetValue;
+    }
+    uint256 public currentSession;
+    mapping(uint => mapping(address => bool)) public hasVoted;
+
+    uint256 public approvalThreshold;
+    uint256 public voteFee;
+    Vote[] public votes;
+    bool public hasActiveVote;
+    VotingRequest[] public votingQueue;
+
     address public stNearTokenAddress;
     IERC20 public stNear;
 
-    constructor(address _stNearTokenAddress,uint256 _approvalThreshold, uint256 _voteFee) {
+    constructor(address _stNearTokenAddress, uint256 _approvalThreshold, uint256 _voteFee) {
+        currentSession = 1;
         approvalThreshold = _approvalThreshold;
         voteFee = _voteFee;
         stNearTokenAddress = _stNearTokenAddress;
@@ -36,23 +43,39 @@ contract Voting {
     }
 
     modifier hasNoActiveVote() {
-        require(!hasActiveVote, "Only coin holders can vote.");
+        require(!hasActiveVote, "There is an active vote.");
         _;
     }
 
     modifier _hasActiveVote() {
-        require(hasActiveVote, "Only coin holders can vote.");
+        require(hasActiveVote, "There is no active vote.");
         _;
     }
 
-    function startVote(address userWallet,uint256 assetValue) public hasNoActiveVote{
-        votes.push(TotalVote(Asset(userWallet,assetValue),0,0));
+    function startVote(address userWallet, uint256 assetValue) internal hasNoActiveVote {
+        votes.push(Vote(Asset(userWallet, assetValue), 0, 0, address(0), false));
         hasActiveVote = true;
     }
 
-    function vote(bool _approval) public _hasActiveVote{
-        require(generatedContract != address(0) && generatedContract != address(1), "There is no active vote.");
-        require(!hasVoted[msg.sender], "You have already voted.");
+    function requestVote(address userWallet, uint256 assetValue) public {
+        require(!hasVoted[currentSession][msg.sender], "You have already voted.");
+        votingQueue.push(VotingRequest(userWallet, assetValue));
+    
+        if (!hasActiveVote) {
+            processNextVoteRequest();
+        }
+    }   
+
+    function processNextVoteRequest() internal {
+        require(!hasActiveVote, "There is an active vote. Please wait for the current vote to complete.");
+        require(votingQueue.length > 0, "Voting queue is empty.");
+        VotingRequest storage nextRequest = votingQueue[0];
+        startVote(nextRequest.userWallet, nextRequest.assetValue);
+        votingQueue.pop();
+    }
+
+    function vote(bool _approval) public _hasActiveVote {
+        require(!hasVoted[currentSession][msg.sender], "You have already voted.");
 
         // Check if the sender has enough stNEAR tokens to pay the fee
         require(stNear.balanceOf(msg.sender) >= voteFee, "Insufficient stNEAR balance for voting.");
@@ -60,15 +83,29 @@ contract Voting {
         // Deduct the vote fee from the sender's stNEAR balance
         stNear.transferFrom(msg.sender, address(this), voteFee);
 
-        hasVoted[msg.sender] = true;
-        votes[votes.length-1].totalVotes +=1;
-        
-        if (votes[votes.length-1].positiveVotes <= approvalThreshold && _approval) {
-            votes[votes.length-1].positiveVotes +=1;
-        }else{
-            address ownerWallet = votes[votes.length-1].asset.ownerWallet;
-            uint256 assetValue = votes[votes.length-1].asset.assetValue;
-            address(SolInsurance(ownerWallet,assetValue));
+        uint256 currentVoteIndex = votes.length - 1;
+        Vote storage currentVote = votes[currentVoteIndex];
+        currentVote.totalVotes += 1;
+
+        if (currentVote.positiveVotes <= approvalThreshold && _approval) {
+            currentVote.positiveVotes += 1;
         }
-    }
+
+        if (currentVote.positiveVotes > approvalThreshold && !currentVote.votingCompleted) {
+            currentVote.votingCompleted = true;
+            address ownerWallet = currentVote.asset.ownerWallet;
+            uint256 assetValue = currentVote.asset.assetValue;
+            currentVote.insuranceContract = address(new SolInsurance(ownerWallet, assetValue));
+            hasActiveVote = false;
+            currentSession++;
+
+            
+            if (votingQueue.length > 0) {
+                processNextVoteRequest();
+            }
+        }
+
+        hasVoted[currentSession][msg.sender] = true;
+    } 
+
 }
