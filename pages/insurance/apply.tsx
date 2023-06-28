655a6417b7d8946907ad23c4c8247a90b3a39505
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-06-28 16:21:00 +0300

    Form controls

--- pages/insurance/apply.tsx
+++ pages/insurance/apply.tsx
@@ -2,12 +2,12 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import Page from "../../components/layout/page";
 import CustomTitle from "../../components/title/title";
 import { Web3Storage } from "web3.storage";
-import axios from "axios";
 import { useRouter } from "next/router";
 
 
 const ApplyForVote = () => {
     const [userFile, setSelectedFile] = useState<File | null>(null);
+    const [assetValue, setAssetValue] = useState<number>(0)
     const [isUploaded, setIsUploaded] = useState<boolean>(false);
     const router = useRouter();
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -15,8 +15,19 @@ const ApplyForVote = () => {
             setSelectedFile(event.target.files[0]);
         }
     };
+    const handleAssetValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        try{
+            if (event.target.value && parseInt(event.target.value) > 0) {
+                setAssetValue(parseInt(event.target.value));
+            }
+        }catch{
+
+        }
+        
+    }
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        //send vote
         try {
             if (userFile) {
                 const client = new Web3Storage({ token: process.env.NEXT_PUBLIC_WEB3_STORAGE_API_KEY ?? "" });
@@ -35,6 +46,7 @@ const ApplyForVote = () => {
                 <div className="form-control mt-20 w-full max-w-xs">
                     <form className="w-full" onSubmit={handleSubmit}>
                         <input type="file" onChange={handleFileChange} className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
+                        <input type="text" onChange={handleAssetValueChange} value={assetValue} placeholder="Asset Value" className="input input-bordered input-info mt-10 w-full max-w-xs" />
                         <button className="btn btn-secondary w-full mt-20" type="submit">Apply</button>
                     </form>
                 </div>
--- pages/insurance/vote.tsx
+++ pages/insurance/vote.tsx
@@ -1,22 +1,38 @@
+import { useEffect, useState } from "react";
 import Page from "../../components/layout/page";
 import CustomTitle from "../../components/title/title";
 
 const Vote = () => {
+    const [cid,setCid] = useState<string>("");
+
+    useEffect(()=>{
+        // get Vote
+    },[])
+
+    const sendNegativeVote = () =>{
+        
+    }
+
+    const sendPositiveVote = () =>{
+        
+    }
     return (
         <Page>
             <CustomTitle title="Hello, Let&apos;s Vote" />
             <div className="flex w-full h-96 mt-20">
-                <div className="w-1/2 h-full bg-black mx-10">
-                    A
-                </div>
-                <div className="col-span-12 w-1/2 h-full mx-10">
+                <div className="col-span-12 w-full h-full mx-10">
                     <div className="flex w-full text-black justify-center mt-10">
                         Asset Value : 1200stNear
+                    </div>
+                    <div className="flex w-full text-black justify-center mt-10">
                         Wallet Address: 0x1111
                     </div>
+                    <div className="flex w-full text-black justify-center mt-10">
+                        https://dweb.link/ipfs/${cid}
+                    </div>
                     <div className="flex w-96 justify-around mt-20 mx-auto">
-                        <button className="btn btn-error">No</button>
-                        <button className="btn btn-success">Yes</button>
+                        <button onClick={sendNegativeVote} className="btn btn-error">No</button>
+                        <button onClick={sendPositiveVote} className="btn btn-success">Yes</button>
                     </div>
                 </div>
             </div>
